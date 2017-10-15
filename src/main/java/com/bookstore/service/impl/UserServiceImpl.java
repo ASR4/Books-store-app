1bf274a975d7cf95f389f37e562e7527de837544
Ashmeet Rekhi <ashrekhi@gmail.com>
2017-10-14 18:46:05 -0700

    Created user logic

--- src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -1,7 +1,6 @@
 package com.bookstore.service.impl;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import org.slf4j.Logger;
@@ -10,12 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 import com.bookstore.domain.User;
-
 import com.bookstore.domain.security.PasswordResetToken;
 import com.bookstore.domain.security.UserRole;
-
+import com.bookstore.repository.RoleRepository;
 import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 
@@ -26,6 +23,9 @@ public class UserServiceImpl implements UserService{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Override
 	public User findByUsername(String username) {
@@ -61,11 +61,24 @@ public class UserServiceImpl implements UserService{
 	}
 
 	@Override
-	public User createUser(User user, Set<UserRole> userRoles) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public User createUser(User user, Set<UserRole> userRoles){
+		User localUser = userRepository.findByUsername(user.getUsername());
+		
+		if(localUser != null) {
+			LOG.info("user {} already exists. Nothing will be done.", user.getUsername());
+		} else {
+			for (UserRole ur : userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			
+			user.getUserRoles().addAll(userRoles);
+			
+			localUser = userRepository.save(user);
+		}
+		
+		return localUser;
 	}
-
 	@Override
 	public void setUserDefaultPayment(Long userPaymentId, User user) {
 		// TODO Auto-generated method stub
